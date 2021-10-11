e95b89822de4bd9843e8ea5586997468b3d54906
jonthan <jonathang2812@hotmail.com>
2021-10-11 18:21:23 -0500

    import-img

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import woman from "./assests/img/woman.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <form className="form">
+          <h1>Trouble logging in?</h1>
+          <h2>Enter your email or phone number below</h2>
+          <div className="container">
+            <div className="input-container">
+              <input type="text" placeholder="Phone or Email" />
+            </div>
+            <input type="submit" value="Continue" className="button" />
+            <p>
+              <u>don't remember anything :(</u>
+            </p>
+          </div>
+        </form>
+        <div>
+          <img src={woman} className="img" alt="woman" />
+        </div>
+      </div>
     </div>
   );
 }
