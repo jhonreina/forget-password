69d386dac4e54db29d8102e4d5c2f72592bd0c96
jonthan <jonathang2812@hotmail.com>
2021-10-14 16:59:24 -0500

    whoProcess

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./components/recoverPassword/App.css";
 
 import Form from "./components/recoverPassword/Form";
-import Login from './components/loginProcess/Login'
+import Login from './components/loginProcess/Login';
+import Who from './components/WhoProcess/Who';
+
 function App() {
   return (
     <div className="App">
       <Form />
       <Login />
+      <Who />
     </div>
   );
 }
